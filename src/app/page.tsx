c3c4a51079a00d6335ce34a4e209fcba786a80a7
Anorak <anorak@pop-os.localdomain>
2024-04-11 18:15:28 +0530

    +Blog links, -pricer, -legal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,35 +58,32 @@ export default function Home() {
         {/* Text Content */}
         <div className="px-4 py-20 md:px-0" style={{ position: 'relative', zIndex: 1 }}>
           <h1 className="text-6xl font-bold mb-4 text-white">Own Your Money</h1>
-          <p className="text-2xl text-white">Subtitle goes here</p>
+          <p className="text-2xl text-white">The best place to buy Bitcoin.
+          </p>
         </div>
       </section>
 
 
+
       <section className="bg-custom-black ">
         {/* Pricer Placeholder */}
-        <section className="container mx-auto my-12 px-4 md:px-0"> {/* Add padding here */}
-          <h2 className="text-3xl text-center font-bold mb-6">Our Pricing</h2>
-          <div className="bg-gray-100 p-8 rounded-lg shadow-md">
-            <p className="text-black">Pricer component will go here</p>
-          </div>
-        </section>
+
 
         {/* Blog Section */}
         <section className="container mx-auto my-12 px-4 md:px-0"> {/* Add padding here */}
           <h2 className="text-3xl text-center font-bold mb-6">From Our Blog</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            <a href="https://example.com/blog/post1" className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-              <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Blog Post Title 1</h3>
-              <p className="font-normal text-gray-700">Description of the blog post...</p>
+            <a href="https://www.bullbitcoin.com/blog/bull-bitcoin-prime-services-for-bitcoin-investors-pdf-guide" target="_blank" rel="noopener noreferrer" className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+              <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Introducing the Bull Bitcoin Prime PDF Guide for Sovereign Investors</h3>
+              <p className="font-normal text-gray-700">Get the Bull Bitcoin Prime PDF; an invaluable resource for Sovereign Individuals, Corporations, Early Adopters, and Professionals</p>
             </a>
-            <a href="https://example.com/blog/post1" className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-              <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Blog Post Title 2</h3>
-              <p className="font-normal text-gray-700">Description of the blog post...</p>
+            <a href="https://www.bullbitcoin.com/blog/why-bull-bitcoin-is-immune-from-ftx-collapse-and-crypto-contagion" target="_blank" rel="noopener noreferrer" className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+              <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Why Bull Bitcoin is immune from FTX collapse and “crypto contagion”</h3>
+              <p className="font-normal text-gray-700">In this article, I will explain why Bull Bitcoin is the safest Bitcoin exchange in Canada to buy and sell Bitcoin, and potentially one of the safest online Bitcoin exchange in the world.</p>
             </a>
-            <a href="https://example.com/blog/post1" className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-              <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Blog Post Title 3</h3>
-              <p className="font-normal text-gray-700">Description of the blog post...</p>
+            <a href="https://www.bullbitcoin.com/blog/bitcoinsupportcom-by-bull-bitcoin-a-call-center-for-wallet-self-custody" target="_blank" rel="noopener noreferrer" className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+              <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">BitcoinSupport.com by Bull Bitcoin: A Call Center for Wallet Self-Custody</h3>
+              <p className="font-normal text-gray-700">Bitcoin Support by Bull Bitcoin is a new service designed to help anybody install, secure and manage their own Bitcoin Wallets with 1-on-1 phone calls and ongoing support calls. Available worldwide!</p>
             </a>
           </div>
         </section>
@@ -100,7 +97,7 @@ export default function Home() {
           <div className="mb-4 md:mb-0 w-full md:w-auto">
             <div className="flex flex-col items-center md:items-start">
               <div className="flex items-center mb-2">
-                <Image src="/logos/red-bull.png" alt="Logo" width={40} height={40} />
+                <Image src="/logos/red-bull.png" alt="Logo" width={50} height={50} />
                 <span className="ml-2 font-semibold">Bull Bitcoin India</span>
               </div>
               <div className="flex justify-center md:justify-start space-x-4">
@@ -121,19 +118,7 @@ export default function Home() {
           </div>
 
           {/* Right Side - Legal */}
-          <div className="mt-4 md:mt-0">
-            <p className="font-semibold mb-2">Legal</p>
-            <div className="block">
-              <Link href="/terms" className="border-b border-0 hover:border-red-700 border-transparent">
-                Terms and Conditions
-              </Link>
-            </div>
-            <div className="block hover">
-              <Link href="/terms" className="border-b border-0 hover:border-red-700 border-transparent">
-                Privacy Policy
-              </Link>
-            </div>
-          </div>
+
         </div>
 
         {/* Divider */}
@@ -152,3 +137,26 @@ export default function Home() {
 
 
 
+//   {/* Pricer Placeholder */}
+//   <section className="container mx-auto my-12 px-4 md:px-0"> {/* Add padding here */}
+//   <h2 className="text-3xl text-center font-bold mb-6">Our Pricing</h2>
+//   <div className="bg-gray-100 p-8 rounded-lg shadow-md">
+//     <p className="text-black">Pricer component will go here</p>
+//   </div>
+// </section>
+
+
+// {/* Right Side - Legal */}
+// <div className="mt-4 md:mt-0">
+//   <p className="font-semibold mb-2">Legal</p>
+//   <div className="block">
+//     <Link href="/terms" className="border-b border-0 hover:border-red-700 border-transparent">
+//       Terms and Conditions
+//     </Link>
+//   </div>
+//   <div className="block hover">
+//     <Link href="/terms" className="border-b border-0 hover:border-red-700 border-transparent">
+//       Privacy Policy
+//     </Link>
+//   </div>
+// </div>
